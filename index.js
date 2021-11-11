0508f9ceeddc7adfceff7058939f753ddf7264f6
guiguicdd <50055027+guiguicdd@users.noreply.github.com>
2021-11-11 12:07:18 -0300

    v1.0.005 - beta

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-// De 104 para 57
-const makeWASocket = require('@adiwajshing/baileys-md').default
-const { BufferJSON, initInMemoryKeyStore, DisconnectReason } = require('@adiwajshing/baileys-md')
+// De 104 para 51
+const { default: makeWASocket, BufferJSON, initInMemoryKeyStore, DisconnectReason } = require("@adiwajshing/baileys-md")
 const fs = require('fs')
-const connfile = './wabasemdConnection.json'
+const connfile = './wabasemdConnection.json';
 
-async function startwabasemd() {
+(async () => {
     let sock = undefined
 
     const loadState = () => {
@@ -19,20 +18,18 @@ async function startwabasemd() {
     const saveState = (state) => {
         console.log('+ saving pre-keys')
         state = state || sock?.authState
-        var towrite = JSON.stringify(state, BufferJSON.replacer, 2)
-        fs.writeFileSync(connfile, towrite)
+        fs.writeFileSync(connfile, JSON.stringify(state, BufferJSON.replacer, 2))
     }
 
     const startSock = () => {
         const sock = makeWASocket({ printQRInTerminal: true, auth: loadState() })
         sock.ev.on('messages.upsert', async m => {
             const msg = m.messages[0]
-            if (!msg) return
-            if (!msg.message || msg.key.fromMe) return
+            if (!msg?.message || msg.key.fromMe) return
             if (m.type === 'notify') {
-                console.log('+ respondendo: ', m.messages[0].key.remoteJid)
+                console.log('+ respondendo: ', msg.key.remoteJid)
                 await sock.sendReadReceipt(msg.key.remoteJid, msg.key.participant, [msg.key.id])
-                await sock.sendMessage(msg.key.remoteJid, { text: 'Fala meu bom! WABaseMD funcionando perfeitamente!' })
+                await sock.sendMessage(msg.key.remoteJid, { text: 'Opa! WABaseMD funcionando!' })
             }
         })
         return sock
@@ -43,15 +40,12 @@ async function startwabasemd() {
     sock.ev.on('connection.update', (update) => {
         const { connection, lastDisconnect } = update
         if (connection === 'close') {
-            const Reconnect = (lastDisconnect.error)?.output?.statusCode !== DisconnectReason.loggedOut
-            Reconnect ? sock = startSock() && console.log('+ Reconnectando...')
+            lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut
+                ? sock = startSock() && console.log('+ Reconnectando...')
                 : fs.unlinkSync(connfile) && console.log("+ Logged Out")
-            console.log('+ connection closed due to ', lastDisconnect.error, ' reconnecting ', Reconnect)
         }
         console.log('+ connection update', update)
     })
 
     sock.ev.on('auth-state.update', () => saveState())
-}
-
-startwabasemd()
+})()
