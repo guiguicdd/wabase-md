9e16f198db0abfe92f8be764151ad816978d18f7
Ericky Thierry <erickythierry@gmail.com>
2021-11-21 17:32:37 -0300

    update auth function

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
 // De 104 para 47
-const { default: makeWASocket, BufferJSON, initInMemoryKeyStore, DisconnectReason } = require("@adiwajshing/baileys-md")
+const { default: makeWASocket, BufferJSON, initInMemoryKeyStore, DisconnectReason, useSingleFileAuthState } = require("@adiwajshing/baileys-md")
 const fs = require('fs')
 const connfile = './wabasemdConnection.json';
 
-const loadState = () => {
-    let state = undefined
-    try {
-        const value = JSON.parse(fs.readFileSync(connfile, { encoding: 'utf-8' }), BufferJSON.reviver)
-        state = { creds: value.creds, keys: initInMemoryKeyStore(value.keys) }
-    } catch (e) { console.log('+ ' + e) }
-    return state
-}
-
-const saveState = (state) => {
-    console.log('+ saving pre-keys')
-    fs.writeFileSync(connfile, JSON.stringify(state, BufferJSON.replacer, 2))
-}
+const { state, saveState } = useSingleFileAuthState(connfile)
 
 const startSock = () => {
-    const sock = makeWASocket({ printQRInTerminal: true, auth: loadState() })
+    const sock = makeWASocket({ printQRInTerminal: true, auth: state })
 
     sock.ev.on('messages.upsert', async m => {
         const msg = m.messages[0]
@@ -39,7 +27,8 @@ const startSock = () => {
         console.log('+ connection update', update)
     })
 
-    sock.ev.on('auth-state.update', () => saveState(sock.authState))
+    sock.ev.on('creds.update', saveState)
+    sock.ev.on('auth-state.update', saveState)
 
     return sock
 }
