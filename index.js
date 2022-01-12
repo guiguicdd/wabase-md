8e89d5e761aa8943890dd160bd62db6f6d4ad701
guiguicdd <50055027+guiguicdd@users.noreply.github.com>
2022-01-12 06:51:15 -0300

    small changes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// De 104 para 32
+// De 104 para 19
 const { default: makeWASocket, useSingleFileAuthState, DisconnectReason } = require("@adiwajshing/baileys-md")
 const { state, saveState } = useSingleFileAuthState('./wabasemdConnection.json')
 const { core } = require('./lib')
@@ -7,12 +7,8 @@ const startSock = () => {
     const sock = makeWASocket({ printQRInTerminal: true, auth: state })
     sock.ev.on('connection.update', (update) => {
         const { connection, lastDisconnect } = update
-        if (connection === 'close') {
-            lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut
-                ? startSock()
-                : console.log('+ connection closed')
-        }
-        console.log('+ connection update', update)
+        if (connection === 'close') lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut
+            ? startSock() : console.log('+ connection closed')
     })
     sock.ev.on('creds.update', saveState)
     return sock
