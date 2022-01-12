8e256415f3bb3ce3d291042c7dba4eaeed8c5d17
guiguicdd <50055027+guiguicdd@users.noreply.github.com>
2022-01-12 04:06:55 -0300

    bug fix, add structure, add new examples and more

---
+++ config/configFile.js
@@ -0,0 +1,10 @@
+exports.info = {
+    prefix: ".",
+    dono: {
+        nome: "Guilherme",
+        numero: [
+            "COLOQUE_SEU_NUMERO_AQUI_DESSA_FORMA: 5522981274455@s.whatsapp.net"
+        ]
+    },
+    grupo: ""
+}
--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 // De 104 para 32
 const { default: makeWASocket, useSingleFileAuthState, DisconnectReason } = require("@adiwajshing/baileys-md")
 const { state, saveState } = useSingleFileAuthState('./wabasemdConnection.json')
+const { core } = require('./lib')
 
 const startSock = () => {
     const sock = makeWASocket({ printQRInTerminal: true, auth: state })
-
-    sock.ev.on('messages.upsert', async m => {
-        const msg = m.messages[0]
-        if (!msg.key.fromMe && m.type === 'notify') {
-            console.log('+ respondendo: ', msg.key.remoteJid)
-            await sock.sendReadReceipt(msg.key.remoteJid, msg.key.participant, [msg.key.id])
-            await sock.sendMessage(msg.key.remoteJid, { text: 'Opa! WABaseMD funcionando! Agora a estrela do https://github.com/guiguicdd/wabase-md' })
-        }
-    })
-
     sock.ev.on('connection.update', (update) => {
         const { connection, lastDisconnect } = update
         if (connection === 'close') {
@@ -23,10 +14,10 @@ const startSock = () => {
         }
         console.log('+ connection update', update)
     })
-
     sock.ev.on('creds.update', saveState)
-
     return sock
 }
 
-startSock()
+const sock = startSock()
+
+sock.ev.on('messages.upsert', async m => await core(sock, m))
