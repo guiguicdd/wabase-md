7acf3b7501b7fa8123fa4aeb820c7bd921ffc582
guiguicdd <50055027+guiguicdd@users.noreply.github.com>
2022-04-24 22:06:05 -0300

    add more features and bug fix

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { core } = require('./lib');
 
 createConnection.then(sock => {
     sock.ev.on('messages.upsert', async m => await core(sock, m))
+    // setTimeout(() => process.exit(1), 10800000); Apenas se o bot ficar parando de responder depois de 3h
 })
 
 createConnection.catch(erro => console.log(erro, 'Conn'))
