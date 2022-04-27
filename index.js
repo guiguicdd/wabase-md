f36123a30bfc7a0db78523233a4721c7ac8a55e0
guiguicdd <50055027+guiguicdd@users.noreply.github.com>
2022-04-27 14:01:00 -0300

    update connection

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-const { createConnection } = require('./connection')
-const { core } = require('./lib');
+const { core } = require('./lib')
+const { default: makeWASocket, useSingleFileAuthState, DisconnectReason, fetchLatestBaileysVersion } = require("@adiwajshing/baileys")
+const { connectionFileName } = require("./config/configFile")
+const { state, saveState } = useSingleFileAuthState(connectionFileName())
+const MAIN_LOGGER = require("@adiwajshing/baileys/lib/Utils/logger").default
+const logger = MAIN_LOGGER.child({})
+logger.level = 'trace'
 
-createConnection.then(sock => {
+const startSock = async () => {
+    const { version } = await fetchLatestBaileysVersion()
+    const sock = makeWASocket({ version, printQRInTerminal: true, auth: state })
     sock.ev.on('messages.upsert', async m => await core(sock, m))
-    // setTimeout(() => process.exit(1), 10800000); Apenas se o bot ficar parando de responder depois de 3h
-})
+    sock.ev.on('connection.update', (update) => {
+        const { connection, lastDisconnect } = update
+        if (connection === 'close') lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut
+            ? startSock() : console.log('+ connection closed')
+    })
+    sock.ev.on('creds.update', saveState)
+}
 
-createConnection.catch(erro => console.log(erro, 'Conn'))
+startSock()
